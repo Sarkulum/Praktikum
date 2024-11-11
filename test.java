28e5cb02f0183d086f62e7b9b20be4d78d80b003
Sarkulum <derWolf100@gmx.de>
2024-11-11 13:48:20 +0100

    Test

---
+++ test.java
@@ -0,0 +1,33 @@
+public class test {
+    public static void main(String[] args) {
+        int seitenLänge = 6;
+        //höhe
+        for (int y = 1; y <= seitenLänge; y++) {
+            //breite
+            int höhe = y;
+            int höhe2 = y;
+            String output = "";
+            for (int x = 1; x <= seitenLänge; x++) {
+                while (höhe < seitenLänge){
+                    output += " ";
+                    höhe++;
+                }
+                while (höhe2 > 0){
+                    output += "*";
+                    höhe2--;
+                }
+            }
+            System.out.println(output);
+        }
+    }
+}
+
+/*
+  *
+ **
+***
+
+***
+**
+*
+ */
