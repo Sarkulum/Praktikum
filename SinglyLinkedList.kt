a112c364c62de8161277d063721625476d81ae9f
Sarkulum <derWolf100@gmx.de>
2024-11-18 14:01:58 +0100

    18.11

---
+++ SinglyLinkedList.kt
@@ -0,0 +1,88 @@
+class ListNode(var value: Int, var next: ListNode? = null) {
+    override fun toString(): String {
+        return value.toString()
+    }
+}
+
+class SinglyLinkedList {
+    private var head: ListNode? = null
+
+    fun addFirst(value: Int) {
+        val newNode = ListNode(value, head)
+        head = newNode
+    }
+
+    fun setFirst(value: Int) {
+        if (head == null) {
+            throw NoSuchElementException("List is empty")
+        }
+        head!!.value = value
+    }
+
+    fun removeFirst() {
+        if (head == null) {
+            throw NoSuchElementException("List is empty")
+        }
+        head = head!!.next
+    }
+
+    fun addLast(value: Int) {
+        val newNode = ListNode(value)
+        if (head == null) {
+            head = newNode
+            return
+        }
+        var current = head
+        while (current!!.next != null) {
+            current = current.next
+        }
+        current.next = newNode
+    }
+
+    fun setLast(value: Int) {
+        if (head == null) {
+            throw NoSuchElementException("List is empty")
+        }
+        var current = head
+        while (current!!.next != null) {
+            current = current.next
+        }
+        current.value = value
+    }
+
+    fun getLast(): Int {
+        if (head == null) {
+            throw NoSuchElementException("List is empty")
+        }
+        var current = head
+        while (current!!.next != null) {
+            current = current.next
+        }
+        return current.value
+    }
+
+    fun removeLast() {
+        if (head == null) {
+            throw NoSuchElementException("List is empty")
+        }
+        if (head!!.next == null) {
+            head = null
+            return
+        }
+        var current = head
+        while (current!!.next!!.next != null) {
+            current = current.next
+        }
+        current.next = null
+    }
+
+    fun printList() {
+        var current = head
+        val values = mutableListOf<Int>()
+        while (current != null) {
+            values.add(current.value)
+            current = current.next
+        }
+        println(values.joinToString(" -> "))
+    }
+}
---
+++ TestTest.kt
@@ -0,0 +1,35 @@
+fun main() {
+    val linkedList = SinglyLinkedList()
+
+    println("Adding elements to the list:")
+    linkedList.addFirst(10)
+    linkedList.addLast(20)
+    linkedList.addLast(30)
+    linkedList.printList() // Output: 10 -> 20 -> 30
+
+    println("\nSetting the first element to 15:")
+    linkedList.setFirst(15)
+    linkedList.printList() // Output: 15 -> 20 -> 30
+
+    println("\nSetting the last element to 35:")
+    linkedList.setLast(35)
+    linkedList.printList() // Output: 15 -> 20 -> 35
+
+    println("\nRemoving the first element:")
+    linkedList.removeFirst()
+    linkedList.printList() // Output: 20 -> 35
+
+    println("\nGetting the last element:")
+    val last = linkedList.getLast()
+    println("Last element is: $last") // Output: Last element is: 35
+
+    println("\nRemoving the last element:")
+    linkedList.removeLast()
+    linkedList.printList() // Output: 20
+
+    println("\nAdding multiple elements and printing again:")
+    linkedList.addLast(40)
+    linkedList.addLast(50)
+    linkedList.addFirst(5)
+    linkedList.printList() // Output: 5 -> 20 -> 40 -> 50
+}
